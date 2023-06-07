deebb4e3abc959a2444c6127209d09a1ff7c5899
poo0054 <poo0054@outlook.com>
2023-06-07 16:31:14 +0800

    init

---
+++ test/src/main/java/io/github/poo0054/test/TestApplication.java
@@ -0,0 +1,14 @@
+package io.github.poo0054.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangzhi
+ */
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
